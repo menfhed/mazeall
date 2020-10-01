7ab4ec433bdc4458cf79914e8953c2ffa0a0d9de
Pavel Novikov <p.novikov@itransition.com>
2020-10-02 00:23:44 +0300

    Fix style generator.h

--- generator.h
+++ generator.h
@@ -3,16 +3,17 @@
  *
  *  Created on: Oct 20, 2017
  *      Author: npp
- */
+ *
+ * modifyied - october 2020
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 
 #ifndef GENERATOR_H_
 #define GENERATOR_H_
 
 int main_gen(int x_in, int y_in, int fillprob_in, int r1_in, int r2_in, int reps_in);
-
 
 #endif /* GENERATOR_H_ */
